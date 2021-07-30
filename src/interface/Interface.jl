6a081385ecf8cb66601ad443c38196c61e0774da
Rory Lipkis <rorylipkis@gmail.com>
2021-07-29 21:02:59 -0700

    fixed interface module docstring

--- src/interface/Interface.jl
+++ src/interface/Interface.jl
@@ -1,6 +1,6 @@
 """
 Defines interface between custom simulation and AST framework.
-Supports various levels of observability, as well as .
+Supports various levels of observability and access.
 """
 module Interface
 
