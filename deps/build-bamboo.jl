e89a9e22466cf6105a7386a73869ea8a6a69a8dc
Rory Lipkis <rorylipkis@gmail.com>
2021-08-04 16:21:09 -0700

    remove extra PyCall build for docker

--- deps/build-bamboo.jl
+++ deps/build-bamboo.jl
@@ -1,10 +1,8 @@
 ENV["PYTHON"] = "" # prompts internal Conda installation
 using Pkg
 
-Pkg.add("PyCall")
-Pkg.build("PyCall") # force Conda rebuild
-
 Pkg.add([
+    PackageSpec(name="PyCall"),
     PackageSpec(name="PyPlot"),
     PackageSpec(name="TestReports"),
     PackageSpec(url="https://github.com/sisl/NeuralVerification.jl")
