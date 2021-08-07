0b7e489fb1dd25f896947b9aad268f5513499fe0
Rory Lipkis <rorylipkis@gmail.com>
2021-08-06 17:33:55 -0700

    ContinuumWorld example problem

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using AdaStress
 using AdaStress: SoftActorCritic, Analysis.PolicyValueVerification
+using NBInclude
 using Test
 
 @testset "Interface" begin
@@ -19,5 +20,12 @@ end
         SoftActorCritic.to_cpu!(ac)
         nnet = PolicyValueVerification.policy_network(ac; act_mins=[-1.0], act_maxs=[1.0])
         nnet isa PolicyValueVerification.ExtendedNetwork
+    end
+end
+
+test_envs = ["ContinuumWorld"]
+for env in test_envs
+    @testset "Example: $env" begin
+        @test @nbinclude("$env.ipynb")
     end
 end
