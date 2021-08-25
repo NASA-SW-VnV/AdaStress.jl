48d54f2be03a7a4091b3e492710988d99c054d00
Rory Lipkis <rorylipkis@gmail.com>
2021-08-25 13:17:54 -0700

    modular reward capability

--- src/interface/RL.jl
+++ src/interface/RL.jl
@@ -1,33 +1,77 @@
-# Connects ASTInterface with CommonRLInterface, allowing compatibility with standard RL-based solvers.
-
-function CommonRLInterface.reset!(mdp::ASTMDP)
+"""
+Resets MDP.
+"""
+function reset!(mdp::ASTMDP)
     reset!(mdp.sim)
-    reset!(mdp.heuristic, distance(mdp.sim))
-    return nothing
+    reset!(mdp.reward.heuristic)
 end
 
-CommonRLInterface.actions(mdp::ASTMDP{SampleAction}) = environment(mdp.sim)
+"""
+Returns set of available actions as sampleable object.
+"""
+actions(mdp::ASTMDP{<:State, SampleAction}) = environment(mdp.sim)
+actions(::ASTMDP{<:State, SeedAction}) = UInt32
 
-CommonRLInterface.actions(::ASTMDP{SeedAction}) = UInt32
+"""
+Returns observation of environment.
+"""
+observe(mdp::ASTMDP{ObservableState, <:Action}) = Float32.(observe(mdp.sim))
 
-CommonRLInterface.observe(mdp::ASTMDP) = Float32.(observe(mdp.sim))
-
-function CommonRLInterface.act!(mdp::ASTMDP{SampleAction}, action::Vector{<:Real})
+"""
+Steps simulation and returns log probability of action.
+"""
+function evaluate!(mdp::ASTMDP{<:State, A}, a::A) where A <: SampleAction
     env = environment(mdp.sim)
-	value = unflatten(mdp, action)
-    logp = logprob(env, value, mdp.marginalize)
-    step!(mdp.sim, value)
-
-    event = isevent(mdp.sim)
-    heuristic = mdp.heuristic(distance(mdp.sim))
-	return mdp.reward(logp, event, heuristic, mdp.reward_bonus)
+    val = a.sample
+    logp = logprob(env, val, mdp.reward.marginalize)
+    step!(mdp.sim, val)
+    return logp
 end
 
-function CommonRLInterface.act!(mdp::ASTMDP{SeedAction}, seed::UInt32)
-    logp = @fix seed step!(mdp.sim)
-    event = isevent(mdp.sim)
-    heuristic = mdp.heuristic(distance(mdp.sim))
-	return mdp.reward(logp, event, heuristic, mdp.reward_bonus)
+function evaluate!(mdp::ASTMDP{<:State, A}, a::A) where A <: SeedAction
+    copy!(RNG_TEMP, mdp.rng)
+    Random.seed!(mdp.rng, a.seed)
+    logp = step!(mdp.rng, mdp.sim)
+    copy!(mdp.rng, RNG_TEMP)
+    return logp
 end
 
-CommonRLInterface.terminated(mdp::ASTMDP) = isterminal(mdp.sim) || isevent(mdp.sim)
+"""
+Applies raw action to environment and returns reward.
+"""
+(mdp::ASTMDP{<:State, <:Action})(action) = (mdp)(convert_a(mdp, action))
+
+"""
+Applies converted action to environment and returns reward.
+"""
+function (mdp::ASTMDP{<:State, A})(a::A) where A <: Action
+    # rewards (partial application)
+    heur = mdp.reward.heuristic(distance(mdp.sim))
+    rew = A <: SampleAction ? reward(mdp.sim, a.sample) : reward(mdp.sim)
+
+    # stepping and likelihood evaluation
+    logp = evaluate!(mdp, a)
+
+    # final reward calculation
+    event = isevent(mdp.sim) ? mdp.reward.event_bonus : 0.0 #TODO: decide on r(s) vs. r(s')
+    r = mdp.reward.reward_function(logp, event, heur(mdp))
+    r += rew isa Function ? rew(mdp.sim) : rew
+    return r
+end
+
+"""
+Returns Boolean indicating termination status.
+"""
+terminated(mdp::ASTMDP) = isterminal(mdp.sim) || isevent(mdp.sim)
+
+# Connects AdaStress interface to CommonRLInterface
+
+CommonRLInterface.reset!(mdp::ASTMDP) = reset!(mdp)
+
+CommonRLInterface.actions(mdp::ASTMDP) = actions(mdp)
+
+CommonRLInterface.observe(mdp::ASTMDP) = observe(mdp)
+
+CommonRLInterface.act!(mdp::ASTMDP, action) = (mdp)(action)
+
+CommonRLInterface.terminated(mdp::ASTMDP) = terminated(mdp)
