05a9fb5a26a7548488372dd6db78c483d9539687
Rory Lipkis <rorylipkis@gmail.com>
2021-09-07 17:43:23 -0700

    changed MCS from step- to episode-based

--- src/solvers/local/mcs/MonteCarloSearch.jl
+++ src/solvers/local/mcs/MonteCarloSearch.jl
@@ -8,35 +8,30 @@ using ..Solvers
 import ..Solvers.solve
 
 using CommonRLInterface
-using DataStructures: PriorityQueue
+using DataStructures: PriorityQueue, enqueue!
 using ProgressMeter
 
 Base.@kwdef mutable struct MCS <: LocalSolver
-    num_steps::Int64=10000
-    top_k::Int64=10
+    num_iterations::Int64 = 1000
+    top_k::Int64          = 10
 end
 
 function Solvers.solve(mcs::MCS, env_fn::Function)
     mdp = env_fn()
     A = typeof(rand(actions(env_fn())))
-
-    reset!(mdp)
-    path = A[]
     best_paths = PriorityQueue()
-    ret = 0.0
 
-    @showprogress for _ in 1:mcs.num_steps
-        a = rand(actions(mdp))
-        push!(path, a)
-        ret += act!(mdp, a)
-        if terminated(mdp)
-            best_paths[deepcopy(path)] = ret
-            if length(best_paths) > mcs.top_k
-                delete!(best_paths, first(keys(best_paths)))
-            end
-            empty!(path)
-            ret = 0.0
-            reset!(mdp)
+    @showprogress for _ in 1:mcs.num_iterations
+        reset!(mdp)
+        path = A[]
+        r = 0.0
+        d = false
+        while !d
+            d = terminated(mdp)
+            a = rand(actions(mdp))
+            push!(path, a)
+            r += act!(mdp, a)
+            d && enqueue!(best_paths, path, r, mcs.top_k)
         end
     end
 
