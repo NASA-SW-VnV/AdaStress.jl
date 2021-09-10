f6c8025e8b2afc37f3b8590412fc0f88a7d14270
Rory Lipkis <rorylipkis@gmail.com>
2021-09-10 12:32:18 -0700

    switched heuristics to taking ASTMDP argument for consistency/efficiency

--- src/interface/heuristics.jl
+++ src/interface/heuristics.jl
@@ -4,23 +4,29 @@ Custom heuristics can inherit this type and implement `reset!` and application f
 """
 abstract type AbstractDistanceHeuristic end
 reset!(::AbstractDistanceHeuristic) = nothing
+(::AbstractDistanceHeuristic)(::ASTMDP) = unimplemented()
 
 """
 Gradient of conservative potential. Default and recommended.
 """
 struct GradientHeuristic <: AbstractDistanceHeuristic end
-(::GradientHeuristic)(d::Float64) = @defer d - distance(mdp.sim)
+
+function (::GradientHeuristic)(mdp::ASTMDP)
+    d = distance(mdp.sim)
+    @defer d - distance(mdp′.sim)
+end
 
 """
 Minimum distance across episode. Warning: non-Markovian.
 """
-Base.@kwdef struct MinimumHeuristic <: AbstractDistanceHeuristic
+Base.@kwdef mutable struct MinimumHeuristic <: AbstractDistanceHeuristic
     d_min::Float64 = Inf
 end
 
-function (h::MinimumHeuristic)(d::Float64)
-    h.d_min = min(h.d_min, d)
-    @defer -h.d_min
+function (h::MinimumHeuristic)(mdp::ASTMDP)
+    h.d_min = min(h.d_min, distance(mdp.sim))
+    done = terminated(mdp)
+    @defer done ? -h.d_min : 0.0
 end
 
 function reset!(h::MinimumHeuristic)
@@ -31,4 +37,4 @@ end
 Null heuristic. Returns zero.
 """
 struct NullHeuristic <: AbstractDistanceHeuristic end
-(::NullHeuristic)(::Float64) = @defer 0.0
+(::NullHeuristic)(::ASTMDP) = @defer 0.0
