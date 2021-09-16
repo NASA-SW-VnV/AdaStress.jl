39fb7b05365935ac79642a94336443ba3cf92c6c
Rory Lipkis <rorylipkis@gmail.com>
2021-09-16 14:20:11 -0700

    removed explicit neuralverification add from bamboo instructions

--- deps/build-bamboo.jl
+++ deps/build-bamboo.jl
@@ -2,8 +2,7 @@ using Pkg
 
 Pkg.add([
     PackageSpec(name="LocalCoverage"),
-    PackageSpec(name="TestReports"),
-    PackageSpec(url="https://github.com/sisl/NeuralVerification.jl")
+    PackageSpec(name="TestReports")
 ])
 
 Pkg.develop(path=joinpath(@__DIR__, ".."))
