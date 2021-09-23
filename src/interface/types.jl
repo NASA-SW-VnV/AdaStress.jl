be408883266a0ee1344d1f0c63d4ef85ec7588d8
Rory Lipkis <rorylipkis@gmail.com>
2021-09-23 11:59:46 -0700

    moved episodic flag to ASTMDP; fixed environment typing bug

--- src/interface/types.jl
+++ src/interface/types.jl
@@ -13,7 +13,7 @@ unimplemented() = throw(UnimplementedError())
 """
 Stores distributions of environment variables.
 """
-const Environment = Dict{Symbol, Sampleable}
+const Environment = Dict{Symbol, <:Sampleable}
 
 """
 Stores values of environment samples.
@@ -88,6 +88,7 @@ Standard MDP object for AST. Wraps simulation and contains auxiliary information
 Base.@kwdef mutable struct ASTMDP{S<:State, A<:Action} <: AbstractASTMDP{S, A}
 	sim::AbstractSimulation						      # simulation wrapping system under test
     reward::AbstractReward    = Reward()              # reward structure
+    episodic::Bool            = false                 # episodic evaluation
 	env_info::EnvironmentInfo = EnvironmentInfo()	  # inferred environment properties
     rng::AbstractRNG          = Random.default_rng()  # RNG used for simulation
 end
