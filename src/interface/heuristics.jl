d3765b2f73c141c69a5dca2339ebf962315379d8
Rory Lipkis <rorylipkis@gmail.com>
2021-09-23 12:05:44 -0700

    added `apply!` function for heuristics, automatic functoid use

--- src/interface/heuristics.jl
+++ src/interface/heuristics.jl
@@ -1,19 +1,21 @@
 """
 Distance heuristic abstract type.
-Custom heuristics can inherit this type and implement `reset!` and application functions.
+Custom heuristics can inherit this type and implement `reset!` and `apply!` functions.
+Application can return value or partially-applied function transforming f(s,s') to f(s)(s').
 """
 abstract type AbstractDistanceHeuristic end
 reset!(::AbstractDistanceHeuristic) = nothing
-(::AbstractDistanceHeuristic)(::ASTMDP) = unimplemented()
+apply!(::AbstractDistanceHeuristic, ::ASTMDP) = unimplemented()
+(adh::AbstractDistanceHeuristic)(mdp::ASTMDP) = Functoid(apply!(adh, mdp))
 
 """
 Gradient of conservative potential. Default and recommended.
 """
 struct GradientHeuristic <: AbstractDistanceHeuristic end
 
-function (::GradientHeuristic)(mdp::ASTMDP)
+function apply!(::GradientHeuristic, mdp::ASTMDP)
     d = distance(mdp.sim)
-    @defer d - distance(mdp′.sim)
+    return mdp′::ASTMDP -> d - distance(mdp′.sim)
 end
 
 """
@@ -23,10 +25,9 @@ Base.@kwdef mutable struct MinimumHeuristic <: AbstractDistanceHeuristic
     d_min::Float64 = Inf
 end
 
-function (h::MinimumHeuristic)(mdp::ASTMDP)
+function apply!(h::MinimumHeuristic, mdp::ASTMDP)
     h.d_min = min(h.d_min, distance(mdp.sim))
-    done = terminated(mdp)
-    @defer done ? -h.d_min : 0.0
+    return terminated(mdp) ? -h.d_min : 0.0
 end
 
 function reset!(h::MinimumHeuristic)
@@ -34,17 +35,14 @@ function reset!(h::MinimumHeuristic)
 end
 
 """
-Retroactive minimum distance across episodic, for episodic MDPs.
+Final distance encountered. If MDP is episodic, this is equal to the minimum distance
+(determined retroactively instead of accumulated as in MinimumHeuristic).
 """
-struct MissHeuristic <: AbstractDistanceHeuristic end
-
-function (::MissHeuristic)(mdp::ASTMDP)
-    d_miss = isterminal(mdp.sim) ? missdistance(mdp.sim) : 0.0
-    @defer d_miss
-end
+struct FinalHeuristic <: AbstractDistanceHeuristic end
+apply!(::FinalHeuristic, mdp::ASTMDP) = isterminal(mdp.sim) ? -distance(mdp.sim) : 0.0
 
 """
 Null heuristic. Returns zero.
 """
 struct NullHeuristic <: AbstractDistanceHeuristic end
-(::NullHeuristic)(::ASTMDP) = @defer 0.0
+apply!(::NullHeuristic, ::ASTMDP) = 0.0
